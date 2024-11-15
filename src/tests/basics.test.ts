3b5c9bfff180840476ae1384a62ca1a013146ce3
contactpunit <contactpunitjain@gmail.com>
2024-11-15 15:05:21 +0530

    changed to respective test suite for better insights about each testcase run

--- src/tests/basics.test.ts
+++ src/tests/basics.test.ts
@@ -12,20 +12,43 @@ describe('basics test suite', () => {
         // assert
         expect(actual).toBe(expected)
     })
+})
 
-    it.only('shuld return info with type stringInfo for a valid string', () => {
+describe('getStringInfo for argument Test-String', () => {
+    it('should match lowercase', () => {
         const actual = getStringInfo('Test-String')
-
         expect(actual.lower).toBe('test-string')
+    })
+
+    it('should match additionalInfo object', () => {
+        const actual = getStringInfo('Test-String')
         expect(actual.additionalInfo).toEqual({})
-        expect(actual.characters).toEqual(['T', 'e', 's', 't', '-', 'S', 't', 'r', 'i', 'n', 'g'])
+    })
+
+    it('should match length', () => {
+        const actual = getStringInfo('Test-String')
         expect(actual.characters).toHaveLength(11)
+    })
+
+    it('should contain -', () => {
+        const actual = getStringInfo('Test-String')
+        expect(actual.characters).toContain<string>('-')
+    })
+
+    it('characters should be equal to array of characters', () => {
+        const actual = getStringInfo('Test-String')
+        expect(actual.characters).toEqual(['T', 'e', 's', 't', '-', 'S', 't', 'r', 'i', 'n', 'g'])
         expect(actual.characters).toContain<string>('-')
         expect(actual.characters).toEqual(
             expect.arrayContaining(['S', 't', 'r', 'i', 'n', 'g', 'T', 'e', 's', 't', '-'])
         )
+    })
+
+    it('sadditionalInfo hould be defined', () => {
+        const actual = getStringInfo('Test-String')
         expect(actual.additionalInfo).not.toBe(undefined)
         expect(actual.additionalInfo).toBeDefined()
         expect(actual.additionalInfo).not.toBeUndefined()
     })
+
 })
