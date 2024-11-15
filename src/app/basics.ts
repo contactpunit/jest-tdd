af0026e31fca060f8c93d9100170151bf2b75585
contactpunit <contactpunitjain@gmail.com>
2024-11-15 14:44:06 +0530

    Jest matchers and assertions

--- src/app/basics.ts
+++ src/app/basics.ts
@@ -1,3 +1,22 @@
 export function toUpper(arg: string) {
     return arg.toUpperCase()
 }
+
+export type stringInfo = {
+    lower: string,
+    upper: string,
+    characters: string[],
+    length: number,
+    additionalInfo: Object | undefined
+}
+
+export function getStringInfo(arg: string) : stringInfo {
+    return {
+        lower: arg.toLocaleLowerCase(),
+        upper: arg.toUpperCase(),
+        characters: Array.from(arg),
+        length: arg.length,
+        additionalInfo: {}
+
+    }
+}
--- src/tests/basics.test.ts
+++ src/tests/basics.test.ts
@@ -1,4 +1,4 @@
-import { toUpper } from "../app/basics"
+import { getStringInfo, toUpper } from "../app/basics"
 
 describe('basics test suite', () => {
     it('Should return uppercase', () => {
@@ -11,5 +11,21 @@ describe('basics test suite', () => {
 
         // assert
         expect(actual).toBe(expected)
+    })
+
+    it.only('shuld return info with type stringInfo for a valid string', () => {
+        const actual = getStringInfo('Test-String')
+
+        expect(actual.lower).toBe('test-string')
+        expect(actual.additionalInfo).toEqual({})
+        expect(actual.characters).toEqual(['T', 'e', 's', 't', '-', 'S', 't', 'r', 'i', 'n', 'g'])
+        expect(actual.characters).toHaveLength(11)
+        expect(actual.characters).toContain<string>('-')
+        expect(actual.characters).toEqual(
+            expect.arrayContaining(['S', 't', 'r', 'i', 'n', 'g', 'T', 'e', 's', 't', '-'])
+        )
+        expect(actual.additionalInfo).not.toBe(undefined)
+        expect(actual.additionalInfo).toBeDefined()
+        expect(actual.additionalInfo).not.toBeUndefined()
     })
 })
