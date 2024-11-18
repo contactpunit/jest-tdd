edc00a45018bd213dc422d4b9968ae1073418100
contactpunit <contactpunitjain@gmail.com>
2024-11-18 21:08:34 +0530

    mocking 3rd party library function - in our case v4 of uuid

--- src/tests/mockingModules/doubles.test.ts
+++ src/tests/mockingModules/doubles.test.ts
@@ -1,6 +1,11 @@
+
 jest.mock('../../app/doubles/doubles', () => ({
     ...jest.requireActual('../../app/doubles/doubles'),
     toUpper: () => {return 'ABC'}
+}))
+
+jest.mock('uuid', () => ({
+    v4: () => {return '157c6738-2d80-4900-ac53-2c8abbf80990'}
 }))
 
 import * as doubleModule from '../../app/doubles/doubles'
@@ -14,5 +19,10 @@ describe('module mock tests', () => {
     test('toUpper function test', ()=> {
         const result = doubleModule.toUpper('abc')
         expect(result).toBe('ABC')
+    })
+
+    test('toLower function test with 3rd party uuid lib', ()=> {
+        const result = doubleModule.toLowerWithUuid('ABCD')
+        expect(result).toBe('abcd-157c6738-2d80-4900-ac53-2c8abbf80990')
     })
 })
