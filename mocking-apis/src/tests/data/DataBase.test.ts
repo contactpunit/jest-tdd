76238936b0d0d0af2e05414cfec6c61472ea3dc1
contactpunit <contactpunitjain@gmail.com>
2024-11-19 14:43:07 +0530

    100% cverage for DataBase.ts

--- mocking-apis/src/tests/data/DataBase.test.ts
+++ mocking-apis/src/tests/data/DataBase.test.ts
@@ -25,14 +25,91 @@ describe('database test suite', () => {
     })
 
     test('should get element on 1st insert',  async() => {
+        const insertObj1 = {
+            id: '3333',
+            name: 'punit'
+        }
+        
+        const id1 = await sut.insert(insertObj1)
+
+        const actual = await sut.getBy('id', id1)
+
+        expect(actual).toBe(insertObj1)
+    })
+
+    test('should find the inserted element by name property',  async() => {
+        const insertObj1 = {
+            id: '3333',
+            name: 'punit'
+        }
+        const insertObj2 = {
+            id: '4444',
+            name: 'nidhi'
+        }
+        const id1 = await sut.insert(insertObj1)
+        const id2 = await sut.insert(insertObj2)
+
+        const actual = await sut.findAllBy('name', 'nidhi')
+        expect(actual[0].name).toBe('nidhi')
+        const expected = [insertObj2]
+        expect(actual).toEqual(expected)
+    })
+
+    test('should update element property name',  async() => {
         const insertObj = {
             id: '3333',
             name: 'punit'
         }
-        const id = await sut.insert(insertObj)
 
-        const actual = await sut.getBy('id', id)
+        const id1 = await sut.insert(insertObj)
 
-        expect(actual).toBe(insertObj)
+        await sut.update(id1, 'name', 'rohit')
+        const actual = await sut.getBy('id', id1)
+        expect(actual.name).toBe('rohit')
+    })
+
+    test('should delete element by id',  async() => {
+        const insertObj1 = {
+            id: '3333',
+            name: 'punit'
+        }
+        const insertObj2 = {
+            id: '444',
+            name: 'nidhi'
+        }
+        const insertObj3 = {
+            id: '555',
+            name: 'rohit'
+        }
+
+        const id1 = await sut.insert(insertObj1)
+        const id2 = await sut.insert(insertObj2)
+        const id3 = await sut.insert(insertObj3)
+
+        await sut.delete(id3)
+        const actual = await sut.getAllElements()
+        expect(actual.length).toBe(2)
+    })
+
+    test('should findall elements',  async() => {
+        const insertObj1 = {
+            id: '3333',
+            name: 'punit'
+        }
+        const insertObj2 = {
+            id: '444',
+            name: 'nidhi'
+        }
+        const insertObj3 = {
+            id: '555',
+            name: 'rohit'
+        }
+
+        const id1 = await sut.insert(insertObj1)
+        const id2 = await sut.insert(insertObj2)
+        const id3 = await sut.insert(insertObj3)
+
+        const actual = await sut.getAllElements()
+        expect(actual.length).toBe(3)
     })
 })
