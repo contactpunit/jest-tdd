24ab1ad335c1abea1f66a9b163255709a86625ec
contactpunit <contactpunitjain@gmail.com>
2024-11-21 12:55:40 +0530

    added more tests

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -26,3 +26,25 @@ test('should return correctsum if string of numbers provided', () => {
     const result = add(numbers)
     expect(result).toBe(expectedResult)
 })
+
+test('should return 0 if empty array provided', () => {
+    const numbers = []
+    const expectedResult = 0
+    const result = add(numbers)
+    expect(result).toBe(expectedResult)
+})
+
+test('expect exception to be thrown if no iarg provided to add fn', () => {
+    const resultFn = () => {
+        add()
+    }
+
+    expect(resultFn).toThrow()
+})
+
+test('should throw an error if multiple arguments passed instead of array', () => {
+    const resultFn = () => {
+        add(1, 2, 3)
+    }
+    expect(resultFn).toThrow()
+})
