7d7b573826bd028e7fe2b7aeaae07bf7227f26a7
contactpunit <contactpunitjain@gmail.com>
2024-11-21 12:02:30 +0530

    arrange act assert

---
+++ basic-testing-frontend/src/math.test.js
@@ -0,0 +1,15 @@
+import { expect, test } from 'vitest';
+import { add } from './math'
+
+test('should return sum of all numbers in an array', () => {
+
+    // Arrange
+    const numbers = [1, 2,3, 4]
+
+    // Act
+    const result = add(numbers)
+
+    // Assert
+    const expectedResult = numbers.reduce((tot, curr) => tot + curr, 0) 
+    expect(result).toBeCloseTo(expectedResult)
+})
