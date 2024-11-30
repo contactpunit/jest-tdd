faf4d8b6469fec90a7525d9440dd8a47196bd2b9
contactpunit <contactpunitjain@gmail.com>
2024-11-30 13:22:57 +0530

    added new testcases for user class and methods

--- basic-testing-frontend/src/hooks.test.js
+++ basic-testing-frontend/src/hooks.test.js
@@ -7,4 +7,21 @@ describe('class User', () => {
         const user = new User(email)
         expect(user).toBeInstanceOf(User)
     })
+
+    it('should update email address of user object', () => {
+        const email = 'abc@test.com'
+        const user = new User(email)
+        expect(user.email).toBe('abc@test.com')
+        user.email = 'punit@test.com'
+        expect(user.email).toBe('punit@test.com')
+    })
+
+    it('should clear email field for user object', () => {
+        const email = 'abc@test.com'
+        const user = new User(email)
+        expect(user.email).toBe('abc@test.com')
+        user.email = ''
+        expect(user.email).toBeDefined()
+        expect(user.email).toBe('')
+    })
 })
