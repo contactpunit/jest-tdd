959cbb4f3271a3f65e296291cb3dd5305de95c7d
contactpunit <contactpunitjain@gmail.com>
2024-11-30 13:12:44 +0530

    tests returning promises using async await

--- basic-testing-frontend/src/async-example.test.js
+++ basic-testing-frontend/src/async-example.test.js
@@ -1,6 +1,6 @@
 import { describe, it, expect } from 'vitest'
 
-import { generateToken } from './async-example.js'
+import { generateToken, generateTokenPromise } from './async-example.js'
 
 describe('generateToken()', () => {
     it('should generate token', (done) => {
@@ -14,5 +14,18 @@ describe('generateToken()', () => {
                 done(err);
             }
         })
+    })
+
+    it('should generate token - using promises', () => {
+        const emailAddress = 'abc@test.com'
+        expect(generateTokenPromise(emailAddress)).resolves.toBeDefined()
+        expect(generateTokenPromise(emailAddress)).resolves.toBeTypeOf('string')
+    })
+
+    it('should generate token - using promises', async() => {
+        const emailAddress = 'abc@test.com'
+        const token = await generateTokenPromise(emailAddress)
+        expect(token).toBeDefined()
+        expect(token).toBeTypeOf('string')
     })
 })
