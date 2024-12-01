61ab4e9ef17949cbd180d6cc46a260e0bc2988f5
contactpunit <contactpunitjain@gmail.com>
2024-12-01 13:10:31 +0530

    mocked fs module to check if fs.writeFile is called ot not

--- basic-testing-frontend/src/util/io.test.js
+++ basic-testing-frontend/src/util/io.test.js
@@ -1,13 +1,17 @@
 import {describe, it, expect, vi} from 'vitest'
 import writeData from './io'
+import {promises as fs} from 'fs'
 
 describe('writeData()', () => {
 
     vi.mock('fs')
+    
     it('should write data to file', () => {
         const file = 'test.txt'
         const testData = 'this is for testing'
         
-        return expect(writeData(testData, file)).resolves.toBeUndefined()
+        writeData(testData, file)
+        expect(fs.writeFile).toBeCalled()
+        // return expect(writeData(testData, file)).resolves.toBeUndefined()
     })
 } )
