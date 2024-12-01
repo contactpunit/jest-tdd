06860b1321a965d0e501bc5b2189c06920ae1c4e
contactpunit <contactpunitjain@gmail.com>
2024-12-01 13:16:22 +0530

    added new test with mocking functionality of path module join method

--- basic-testing-frontend/src/util/io.test.js
+++ basic-testing-frontend/src/util/io.test.js
@@ -5,13 +5,31 @@ import {promises as fs} from 'fs'
 describe('writeData()', () => {
 
     vi.mock('fs')
-    
+    vi.mock('path', () => {
+        return {
+            default: {
+                join: (...args) => {
+                    return args[args.length - 1]
+                }
+            }
+        }
+    })
+
     it('should write data to file', () => {
         const file = 'test.txt'
         const testData = 'this is for testing'
         
         writeData(testData, file)
         expect(fs.writeFile).toBeCalled()
+        // return expect(writeData(testData, file)).resolves.toBeUndefined()
+    })
+
+    it('should write data to file', () => {
+        const file = 'test.txt'
+        const testData = 'this is for testing'
+        
+        writeData(testData, file)
+        expect(fs.writeFile).toBeCalledWith(file, testData)
         // return expect(writeData(testData, file)).resolves.toBeUndefined()
     })
 } )
