8d048fdd259362eee4692850dd0e2492cc5e06e7
contactpunit <contactpunitjain@gmail.com>
2024-12-01 12:26:34 +0530

    used vi mock to mock fs functions, this fails old test as file is not created

--- basic-testing-frontend/src/util/io.test.js
+++ basic-testing-frontend/src/util/io.test.js
@@ -1,10 +1,13 @@
-import {describe, it, expect} from 'vitest'
+import {describe, it, expect, vi} from 'vitest'
 import writeData from './io'
 
 describe('writeData()', () => {
+
+    vi.mock('fs')
     it('should write data to file', () => {
         const file = 'test.txt'
         const testData = 'this is for testing'
+        
         return expect(writeData(testData, file)).resolves.toBeUndefined()
     })
 } )
