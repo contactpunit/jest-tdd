7b94f60f9f1cad2d4a2f98f827ca25f396d932c7
contactpunit <contactpunitjain@gmail.com>
2024-12-05 18:56:03 +0530

    added test cases for ReservationHandlers POST request

--- mocking-apis/src/tests/app/handlers/ReservationsHandlers.test.ts
+++ mocking-apis/src/tests/app/handlers/ReservationsHandlers.test.ts
@@ -2,7 +2,8 @@ import { IncomingMessage, ServerResponse } from "http";
 import { ReservationsHandler } from "../../../app/handlers/ReservationsHandler";
 import { ReservationsDataAccess } from "../../../app/data/ReservationsDataAccess";
 import { Authorizer } from "../../../app/auth/Authorizer";
-import { HTTP_CODES } from "../../../app/model/ServerModel";
+import { HTTP_CODES, HTTP_METHODS } from "../../../app/model/ServerModel";
+import { Reservation } from "../../../app/model/ReservationModel";
 
 const getRequestBodyMock = jest.fn()
 
@@ -23,11 +24,20 @@ describe('ReservationHandlers test suite', () => {
 
     const responseMock = {
         statusCode: 0,
-        write: jest.fn()
+        write: jest.fn(),
+        writeHead: jest.fn()
     } 
 
     const authorizerMock = {
         validateToken: jest.fn()
+    }
+
+    const reservation1: Reservation = {
+        id: '',
+        room: 'someroom',
+        user: 'someuser',
+        startDate: new Date().toDateString(),
+        endDate: new Date().toDateString()
     }
 
     const createReservationMock = jest.fn()
@@ -68,6 +78,31 @@ describe('ReservationHandlers test suite', () => {
     })
 
     describe('HTTP POST tests', () => {
-        
+
+        let someid;
+
+        beforeEach(() => {
+            request.headers.authorization = '12345'
+            request.method = HTTP_METHODS.POST
+            someid = '2222'
+        })
+
+        it('should call handlePost and make reservation', async () => {
+            getRequestBodyMock.mockResolvedValue(reservation1)
+            createReservationMock.mockResolvedValue(someid)
+            await sut.handleRequest()
+
+            expect(responseMock.statusCode).toBe(HTTP_CODES.CREATED)
+            expect(responseMock.writeHead).toHaveBeenCalledWith(HTTP_CODES.CREATED, { 'Content-Type': 'application/json' })
+            expect(responseMock.write).toHaveBeenCalledWith(JSON.stringify({ reservationId: someid }))
+        })
+
+        it('should call handlePost and fail for invalid reservation with BAD_REQUEST', async () => {
+            getRequestBodyMock.mockResolvedValue({})
+            await sut.handleRequest()
+
+            expect(responseMock.statusCode).toBe(HTTP_CODES.BAD_REQUEST)
+            expect(responseMock.write).toHaveBeenCalledWith(JSON.stringify('Incomplete reservation!'))
+        })
     })
 })
