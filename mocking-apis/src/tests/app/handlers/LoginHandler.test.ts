2580a2fbed7f8bf088f973468e29a8b6c0e19396
Punit Jain <contactpunitjain@gmail.com>
2024-12-05 11:27:30 +0530

    login handler tests

--- mocking-apis/src/tests/app/handlers/LoginHandler.test.ts
+++ mocking-apis/src/tests/app/handlers/LoginHandler.test.ts
@@ -2,9 +2,14 @@ import { IncomingMessage, ServerResponse } from "http";
 import { LoginHandler } from "../../../app/handlers/LoginHandler";
 import { Authorizer } from "../../../app/auth/Authorizer";
 import { Account } from "../../../app/model/AuthModel";
-import { getRequestBody } from "../../../app/utils/Utils";
-import { HTTP_METHODS } from "../../../app/model/ServerModel";
+import { HTTP_CODES, HTTP_METHODS } from "../../../app/model/ServerModel";
+import exp from "constants";
 
+const mockgetRequestBody = jest.fn()
+
+jest.mock('../../../app/utils/Utils', () => ({
+    getRequestBody: () => mockgetRequestBody()
+}))
 describe('LoginHandler class test suite', () => {
     let sut: LoginHandler
 
@@ -19,12 +24,6 @@ describe('LoginHandler class test suite', () => {
     }
 
     const someToken = '12345'
-    
-    const mockgetRequestBody = jest.fn()
-    
-    jest.mock('../../../app/utils/Utils', () => ({
-        getRequestBody: mockgetRequestBody
-    }))
 
     const mockResponse = {
         statusCode: 0,
@@ -48,9 +47,32 @@ describe('LoginHandler class test suite', () => {
         jest.clearAllMocks()
     })
 
-    it('handleRequest method should create and return a token', () => {
+    it('handleRequest method should create and return a token', async() => {
         request.method = HTTP_METHODS.POST
         mockgetRequestBody.mockResolvedValueOnce(someAccount)
         mockAuthorizer.login.mockResolvedValueOnce(someToken)
+        await sut.handleRequest()
+        expect(mockResponse.statusCode).toBe(HTTP_CODES.CREATED)
+        expect(mockResponse.writeHead).toHaveBeenCalledWith(HTTP_CODES.CREATED, { 'Content-Type': 'application/json' })
+        expect(mockResponse.write).toHaveBeenCalledWith(JSON.stringify({token: someToken}))
+        expect(mockAuthorizer.login).toHaveBeenCalledWith(someAccount.userName, someAccount.password)
+    })
+
+    it('handleRequest method should return NOT_FOUND for incorrect password', async() => {
+        request.method = HTTP_METHODS.POST
+        mockgetRequestBody.mockResolvedValueOnce(someAccount)
+        mockAuthorizer.login.mockResolvedValueOnce(undefined)
+        await sut.handleRequest()
+        expect(mockResponse.statusCode).toBe(HTTP_CODES.NOT_fOUND)
+        expect(mockResponse.write).toHaveBeenCalledWith(JSON.stringify('wrong username or password'))
+    })
+
+    it('handleRequest method should return BAD_REQUEST for invalid account', async() => {
+        request.method = HTTP_METHODS.POST
+        mockgetRequestBody.mockResolvedValueOnce({})
+        await sut.handleRequest()
+        expect(mockResponse.statusCode).toBe(HTTP_CODES.BAD_REQUEST)
+        expect(mockResponse.writeHead).toHaveBeenCalledWith(HTTP_CODES.BAD_REQUEST, { 'Content-Type': 'application/json' })
+        expect(mockResponse.write).toHaveBeenCalledWith(JSON.stringify('userName and password required'))
     })
 })
