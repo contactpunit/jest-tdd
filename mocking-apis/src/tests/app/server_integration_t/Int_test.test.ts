b39cf75d6c299fa0e15f2792bf378432260aef63
contactpunit <contactpunitjain@gmail.com>
2024-12-09 12:19:34 +0530

    integration tests for register, login and reservation handler

--- mocking-apis/src/tests/app/server_integration_t/Int_test.test.ts
+++ mocking-apis/src/tests/app/server_integration_t/Int_test.test.ts
@@ -1,3 +1,4 @@
+import { Reservation } from "../../../app/model/ReservationModel";
 import { HTTP_CODES, HTTP_METHODS } from "../../../app/model/ServerModel";
 import { Server } from "../../../app/server/Server";
 import { makeAwesomeRequest } from "./http_client";
@@ -20,6 +21,16 @@ describe('Integration test for Server GET POST calls', () => {
         password: 'somepass'
     }
 
+    let reqToken
+
+    const someReservation: Reservation = {
+        id: '',
+        room: '123',
+        user: 'punit',
+        startDate: new Date().toDateString(),
+        endDate: new Date().toDateString()
+    }
+
     it('should register the user using custom http client lib', async () => {
         const result = await makeAwesomeRequest({
             host: 'localhost',
@@ -32,5 +43,35 @@ describe('Integration test for Server GET POST calls', () => {
         expect(result.statusCode).toBe(HTTP_CODES.CREATED)
         expect(result.body.userId).toBeDefined()
         
+    })
+
+    it('should login the user and send back token', async () => {
+        const result = await makeAwesomeRequest({
+            host: 'localhost',
+            port: 8080,
+            method: HTTP_METHODS.POST,
+            path: '/login'
+
+        }, someUser)
+
+        expect(result.statusCode).toBe(HTTP_CODES.CREATED)
+        expect(result.body.token).toBeDefined()
+        reqToken = result.body.token
+    })
+
+    it('should create a new reservation for the user', async () => {
+        const result = await makeAwesomeRequest({
+            host: 'localhost',
+            port: 8080,
+            method: HTTP_METHODS.POST,
+            path: '/reservation',
+            headers: {
+                authorization: reqToken
+            }
+
+        }, someReservation)
+
+        expect(result.statusCode).toBe(HTTP_CODES.CREATED)
+        expect(result.body.reservationId).toBeDefined
     })
 })
